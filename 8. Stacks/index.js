137e213b84fc1bad3fb21c163be04c590aa0b675
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2020-01-24 18:56:22 +0100

    push()

--- 8. Stacks/index.js
+++ 8. Stacks/index.js
@@ -3,13 +3,18 @@
 class Stack {
 	constructor() {
 		this._storage = {};
+		this._length = 0;
 	}
 	/*
 	 * Adds a new value at the end of the
 	 * stack
-	 * @param {*} value the value to push
+	 * @param {*} value - the value to push
 	 */
-	push() {}
+	push(value) {
+		// TODO: add typechecking and check arguments (edgecases)
+		this._storage[this._length] = value;
+		this._length++;
+	}
 
 	/*
 	 * Removes the value at the end of the stack and returns it
@@ -26,3 +31,17 @@ class Stack {
 const myStack = new Stack();
 
 console.log(myStack);
+
+myStack.push('zero');
+myStack.push('one');
+
+// {_storage: {0:'zero', 1: 'one'}}
+// length: 2
+// }
+
+console.log(myStack);
+
+// myStack.pop();
+// // {_storage: {0:'zero'}}
+// // length: 1
+// // }
