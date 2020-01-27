86d5267c60f89c616263063f29db37f89ce9c451
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2020-01-27 15:04:56 +0100

    implementing

--- 9. Queues/index.js
+++ 9. Queues/index.js
@@ -3,6 +3,8 @@
 class Queue {
 	constructor() {
 		this._storage = {};
+		this._length = 0;
+		this._headIndex = 0;
 	}
 	/*
 	 * Enqueues a new value at the end
@@ -10,16 +12,48 @@ class Queue {
 	 * @param {*} value - the value to
 	 * enqueue
 	 */
-	enqueue() {}
+	enqueue(value) {
+		// TODO: typechecking, input validation
+		this._storage[this._length] = value;
+		this._length++;
+	}
 
 	/*
 	 * Dequeues the value from the beginning of the queue and returns it
 	 * @return {*} the first and oldest value in the queue
 	 */
-	dequeue() {}
+	dequeue() {
+		if (this._length) {
+			const firstVal = this._storage[this._headIndex];
+			delete this._storage[this._headIndex];
+			this._length--;
+			this._headIndex++;
+			return firstVal;
+		}
+	}
 	/*
 	 * Returns the value at the beginning of the queue without removing it from the queue
 	 * @return {*} value the first and oldest value in the queue
 	 */
 	peek() {}
 }
+
+var myQ = new Queue();
+myQ.enqueue('zero');
+myQ.enqueue('one');
+console.log(myQ);
+// {_storage: {0:'zero', 1: 'one'}}
+// length: 2
+//headIndex:0
+
+myQ.dequeue();
+console.log(myQ);
+// {_storage: { 1: 'one'}}
+// length: 1
+//headIndex:1
+
+myQ.enqueue('two');
+console.log(myQ);
+// {_storage: { 1: 'one'}}
+// length: 1
+//headIndex:1
