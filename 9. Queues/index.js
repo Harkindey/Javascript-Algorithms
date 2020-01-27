f4925fe816d8cc6fc8e536972667ae06d5b5296d
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2020-01-27 14:45:21 +0100

    Queue Exercise

---
+++ 9. Queues/index.js
@@ -0,0 +1,25 @@
+/** Class representing a Queue. */
+
+class Queue {
+	constructor() {
+		this._storage = {};
+	}
+	/*
+	 * Enqueues a new value at the end
+	 * of the queue
+	 * @param {*} value - the value to
+	 * enqueue
+	 */
+	enqueue() {}
+
+	/*
+	 * Dequeues the value from the beginning of the queue and returns it
+	 * @return {*} the first and oldest value in the queue
+	 */
+	dequeue() {}
+	/*
+	 * Returns the value at the beginning of the queue without removing it from the queue
+	 * @return {*} value the first and oldest value in the queue
+	 */
+	peek() {}
+}
