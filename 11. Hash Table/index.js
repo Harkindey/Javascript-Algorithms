1e8167a79a5d1ed644a9219760944c31a13cd329
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2020-02-07 23:48:53 +0100

    Handling a collisison

--- 11. Hash Table/index.js
+++ 11. Hash Table/index.js
@@ -1,6 +1,7 @@
 class HashTable {
-	constructor() {
+	constructor(val) {
 		this._storage = [];
+		this._tableSize = val;
 	}
 	/*
 	 * Inserts a new key-value pair
@@ -8,7 +9,14 @@ class HashTable {
 	 * with the value
 	 * @param {*} value - the value to insert
 	 */
-	insert() {}
+	insert(key, value) {
+		const index = this._hash(key, this._tableSize);
+
+		if (!this._storage[index]) this._storage[index] = [];
+		//[0,0,0,[]]
+		this._storage[index].push([key, value]);
+		//[0,0,0,[['a', 1]]]
+	}
 	/*
 	 * Deletes a key-value pair
 	 * @param {string} key - the key associated with the value
@@ -30,13 +38,15 @@ class HashTable {
 	_hash(str, n) {
 		let sum = 0;
 		for (let i = 0; i < str.length; i++) sum += str.charCodeAt(i) * 3;
-
 		return sum % n;
 	}
 }
 
-const myHT = new HashTable();
+const myHT = new HashTable(25);
 
 console.log(myHT);
 
 myHT.insert('a', 1);
+// HashTable {_storage: [0,0,0,['a',1], 0,0,0]}
+myHT.insert('b', 1);
+// HashTable {_storage: [0,0,0,[['a',1], ['b',2]], 0,0,0]}
