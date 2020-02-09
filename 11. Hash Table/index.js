3edf3dad5e500c0416240ecbf17c0b63ed45d1fc
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2020-02-09 10:31:17 +0100

    Retrieve()

--- 11. Hash Table/index.js
+++ 11. Hash Table/index.js
@@ -14,6 +14,7 @@ class HashTable {
 
 		if (!this._storage[index]) this._storage[index] = [];
 		//[0,0,0,[]]
+		// TODO: loop trhrough array and find if key was already inserted
 		this._storage[index].push([key, value]);
 		//[0,0,0,[['a', 1]]]
 	}
@@ -30,8 +31,16 @@ class HashTable {
 	 */
 	// HashTable {_storage: [0,0,0,[['a',1], ['b',2]], 0,0,0]}
 	// Amontized where worst case time complexity isn't consider cause it isnt helpful
+	// the worst case is so unlikely it isnt useful to think about.
 	retrieve(key) {
 		const index = this._hash(key, this._tableSize);
+		const arrayAtIndex = this._storage[index];
+		if (arrayAtIndex) {
+			for (let i = 0; i < arrayAtIndex.length; i++) {
+				const keyValueArray = arrayAtIndex[i];
+				if (keyValueArray[0] === key) return keyValueArray[1];
+			}
+		}
 	}
 	/*
 	 * Hashes string value into an integer that can be mapped to an array index
